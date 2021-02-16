0b48d62183ee7f1a0ecee8ce49e5fda3db931653
AtulUpadhyayTR <atul.upadhyay@thomsonreuters.com>
2021-02-16 00:02:00 -0500

    REST service implementation using spring boot and without any db

--- src/main/java/org/example/controller/TopicController.java
+++ src/main/java/org/example/controller/TopicController.java
@@ -20,12 +20,22 @@ public class TopicController {
     }
 
     @RequestMapping("/topics/{id}")
-    public Optional<Topic> getTopic(@PathVariable String id){
+    public Topic getTopic(@PathVariable String id){
         return topicService.getTopic(id);
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "/topics")
     public void addTopic(@RequestBody Topic topic){
         topicService.addTopic(topic);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/topics/{id}")
+    public void updateTopic(@PathVariable String id,@RequestBody Topic topic){
+        topicService.updateTopic(id, topic);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/topics/{id}")
+    public void deleteTopic(@PathVariable String id){
+        topicService.deleteTopic(id);
     }
 }
--- src/main/java/org/example/service/TopicService.java
+++ src/main/java/org/example/service/TopicService.java
@@ -1,6 +1,6 @@
 package org.example.service;
 
-import org.example.dao.TopicRepository;
+
 import org.example.entity.Topic;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,72 +10,44 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-/**
- * Commented out code is hard coded list and current code use embedded H2 db for Data storage.
- */
+
 @Service
 public class TopicService {
 
-    @Autowired
-    private TopicRepository topicRepository;
 
-//    private List<Topic> topics= new ArrayList<>(Arrays.asList(
-//            new Topic("spring","Spring Framework","Spring Framework Description"),
-//            new Topic("java","Core Java","Core Java Description"),
-//             new Topic("javascript","Java Script","Java Script Description")
-//    ));
+    private List<Topic> topics= new ArrayList<>(Arrays.asList(
+            new Topic("spring","Spring Framework","Spring Framework Description"),
+            new Topic("java","Core Java","Core Java Description"),
+             new Topic("javascript","Java Script","Java Script Description")
+    ));
 
-    /**
-     * It is iterable so we need to convert this into List.
-     * So here it is performing all the operation automatically-- connecting to the db, returning all the rows and converting each of those rows into
-     * the Topic instances and getting it back.
-     * @return
-     */
+
     public List<Topic> getAllTopics(){
-       // return topics;
-        List<Topic> topics=new ArrayList<>();
-        topicRepository.findAll().forEach(topics::add);
-        return topics;
+       return topics;
     }
 
-    /**
-     * Retrieves an entity by its id.
-     *
-     * Params:
-     * id – must not be null.
-     * Returns:
-     * the entity with the given id or Optional#empty() if none found
-     * Throws:
-     * IllegalArgumentException – if id is null.
-     * @param id
-     * @return
-     */
-    public Optional<Topic> getTopic(String id){
-        // return topics.stream().filter(t ->t.getId().equals(id)).findFirst().get();
-        return topicRepository.findById(id);
+
+    public Topic getTopic(String id){
+        return topics.stream().filter(t ->t.getId().equals(id)).findFirst().get();
+
     }
 
     public void addTopic(Topic topic){
-        //topics.add(topic);
-        topicRepository.save(topic);
+        topics.add(topic);
+
     }
 
     public void updateTopic(String id, Topic topic){
-//        for(int i=0; i<topics.size(); i++){
-//            Topic temp= topics.get(i);
-//            if(temp.getId().equals(id)){
-//                topics.set(i,topic);
-//                return;
-//            }
-//        }
-
-        topicRepository.save(topic);
-
+        for(int i=0; i<topics.size(); i++){
+            Topic temp= topics.get(i);
+            if(temp.getId().equals(id)){
+                topics.set(i,topic);
+                return;
+            }
+        }
     }
 
     public void deleteTopic(String id){
-        //topics.removeIf(t-> t.getId().equals(id));
-
-        topicRepository.deleteById(id);
+        topics.removeIf(t-> t.getId().equals(id));
     }
 }

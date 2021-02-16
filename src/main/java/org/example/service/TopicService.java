fb61c700b2ce625a653ac3c9d968feb158c90323
AtulUpadhyayTR <atul.upadhyay@thomsonreuters.com>
2021-02-16 00:46:43 -0500

    Adding all rest API's for Topic class

--- src/main/java/org/example/service/TopicService.java
+++ src/main/java/org/example/service/TopicService.java
@@ -19,12 +19,6 @@ public class TopicService {
     @Autowired
     private TopicRepository topicRepository;
 
-//    private List<Topic> topics= new ArrayList<>(Arrays.asList(
-//            new Topic("spring","Spring Framework","Spring Framework Description"),
-//            new Topic("java","Core Java","Core Java Description"),
-//             new Topic("javascript","Java Script","Java Script Description")
-//    ));
-
     /**
      * It is iterable so we need to convert this into List.
      * So here it is performing all the operation automatically-- connecting to the db, returning all the rows and converting each of those rows into
@@ -32,7 +26,6 @@ public class TopicService {
      * @return
      */
     public List<Topic> getAllTopics(){
-       // return topics;
         List<Topic> topics=new ArrayList<>();
         topicRepository.findAll().forEach(topics::add);
         return topics;
@@ -51,31 +44,19 @@ public class TopicService {
      * @return
      */
     public Optional<Topic> getTopic(String id){
-        // return topics.stream().filter(t ->t.getId().equals(id)).findFirst().get();
-        return topicRepository.findById(id);
+       return topicRepository.findById(id);
     }
 
     public void addTopic(Topic topic){
-        //topics.add(topic);
         topicRepository.save(topic);
     }
 
     public void updateTopic(String id, Topic topic){
-//        for(int i=0; i<topics.size(); i++){
-//            Topic temp= topics.get(i);
-//            if(temp.getId().equals(id)){
-//                topics.set(i,topic);
-//                return;
-//            }
-//        }
-
         topicRepository.save(topic);
 
     }
 
     public void deleteTopic(String id){
-        //topics.removeIf(t-> t.getId().equals(id));
-
-        topicRepository.deleteById(id);
+       topicRepository.deleteById(id);
     }
 }
